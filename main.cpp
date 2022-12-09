fcbe12dcd1109367b780107bd44cb8ff6bc3f15b
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-12-09 17:04:13 -0600

    test matrix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
 
-int main() {
-	std:: cout << "Hello World!" << std::endl;
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+template<typename T, size_t N, size_t M>
+void printArray(T(&mat)[N][M])
+{
+	for (int i = 0; i < N; i ++ ) {
+		for (int j = 0; j < M; j++ ) {
+			cout << mat[i][j] << ' ';
+		}
+		std::cout << std::endl;
+	}
+}
+ 
+int main()
+{
+	const int m = 80, n = 80;
+ 
+	int mat[m][n] = {
+
+	};
+ 
+	printArray(mat);
+ 
 	return 0;
-	
 }
